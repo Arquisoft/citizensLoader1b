8a1d9214ecf864ab6ff016e352b02fbb4cc9e2e4
DavidGG1983 <uo173944@uniovi.es>
2017-02-13 22:41:15 +0100

    evitar la inserción de las cartas en el repositorio

--- src/main/java/es/uniovi/asw/Program.java
+++ src/main/java/es/uniovi/asw/Program.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import es.uniovi.asw.letters.WordLetter;
-//import es.uniovi.asw.letters.PDFLetter;
+//import es.uniovi.asw.letters.WordLetter;
+import es.uniovi.asw.letters.PDFLetter;
 import es.uniovi.asw.letters.WriteLetter;
 import es.uniovi.asw.parser.RList;
 
@@ -35,7 +35,7 @@ public class Program {
 	
 	public void generateLetters() throws IOException{
 		for(Citizen user : citizens){
-			WriteLetter writer = new WordLetter(user.getName(),user.getPassword(),user.getMail()); //el mail será el login del usuario en el sistema
+			WriteLetter writer = new PDFLetter(user.getName(),user.getPassword(),user.getMail()); //el mail será el login del usuario en el sistema
 			writer.write("mediante el presente mail le adjuntamos su usuario y contraseña de inicio de sesión"
 					+ " para nuestra aplicación");
 		}
